76aa41c080bc846fa58c2b8ba3a23a550df48b2a
Marlene Mora <marlenejmora1@gmail.com>
2017-03-02 23:19:13 +0000

    Basics

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
+var Code2College_Students = ["Andrea", "Vanessa", "Cindy", "Sophie", "Natalia", "Yazmeen", "Selena", "Marlene"];
+
+$(studentButton).click(function() {
+    
+    var studentnames = Math.floor(Math.random()*Code2College_Students.length)
+    
+$("#studentDisplay").html(Code2College_Students[studentnames])
 
 
+
+var Code2College_Educators = ["Stephanie", "Jon", "Chris", "Matt"];
+
+$(teacherButton).click(function() {
+
+    var teachernames = Math.floor(Math.random()*Code2College_Educators.length)
+
+$("#teacherDisplay").html(Code2College_Educators[teachernames])
+})
+
+Code2College_Educators.push("Shae");
+
+    
+});
 });
